e41ebc93e64983fce910f60f7d29c6e23e563654
XiaomingDong-NCL <houchencun@163.com>
2020-03-04 04:19:57 +0000

    set demo.html post and get methods, declare randomdna in views.py

--- bioinformaticsapp/views.py
+++ bioinformaticsapp/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from random import choice
+from Bio.Seq import Seq
 
+# transfer the input number to random sequence and store it in randomdna.txt file
+# define request.method == 'get' to get this sequence
 def bioinformatics(request):
-    return render(request,'demo.html')
+    if request.method == 'post':
+        i=request.post.get("integer", None)
+        with open('randomdna.txt', 'a+') as f:
+            sequence = ''
+            for i in range(int(i)):
+                sequence += choice('ATCG')
+            f.write("{}".format(sequence))
+    if request.method == 'get':
+        with open('randomdna.txt', 'r') as f:
+            randomdna=f.readlines()
+    return render(request,'demo.html', {'data': randomdna})
 
