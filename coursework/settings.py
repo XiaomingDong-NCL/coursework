f99ab79373a1cbea3f8a97d70978994750a30700
XiaomingDong-NCL <houchencun@163.com>
2020-03-05 18:35:18 +0000

    installed app: add in bioinformaticsapp and helloapp

--- coursework/settings.py
+++ coursework/settings.py
@@ -6,6 +6,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
+    'bioinformaticsapp',
+    'helloapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
